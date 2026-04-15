2b49895416b22368222b7274ed0b69e9a91192f7
Fedexair437 <finotti.fede@gmail.com>
2026-04-15 08:44:59 +0000

    accidentalmente rimossa esercitazione 1

---
+++ Esercitazione_1/build/CMakeFiles/stampa_arg.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stampa_arg.dir/link.d"
+  "CMakeFiles/stampa_arg.dir/stampa_arg.cpp.o"
+  "CMakeFiles/stampa_arg.dir/stampa_arg.cpp.o.d"
+  "stampa_arg"
+  "stampa_arg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stampa_arg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Esercitazione_1/build/CMakeFiles/stampa_arg.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for stampa_arg.
+# This may be replaced when dependencies are built.
